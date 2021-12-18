f8bbabf3d66a062dbeefaa24ade198ff3a65164a
heitordeve <heitor.developer@outlook.com>
2021-12-18 18:56:58 -0300

    MInor Change - Cleaning up variable files

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3RJexvj6EZ1ASFjyiYCAnn-Ia6vlFBhM",
-  authDomain: "eventos-93f41.firebaseapp.com",
-  projectId: "eventos-93f41",
-  storageBucket: "eventos-93f41.appspot.com",
-  messagingSenderId: "12895592682",
-  appId: "1:12895592682:web:637b05a045fd70f0e87793"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
