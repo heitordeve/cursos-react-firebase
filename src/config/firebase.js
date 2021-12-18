73f5d6c9283b18dfe374f74d9f1edd489858327d
heitordeve <heitor.developer@outlook.com>
2021-12-18 17:17:02 -0300

    Dev - Authenticating with firebase and testing validations

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC3RJexvj6EZ1ASFjyiYCAnn-Ia6vlFBhM",
-    authDomain: "eventos-93f41.firebaseapp.com",
-    projectId: "eventos-93f41",
-    storageBucket: "eventos-93f41.appspot.com",
-    messagingSenderId: "12895592682",
-    appId: "1:12895592682:web:637b05a045fd70f0e87793"
-  };
-  
-  // Initialize Firebase
-  export default firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyC3RJexvj6EZ1ASFjyiYCAnn-Ia6vlFBhM",
+  authDomain: "eventos-93f41.firebaseapp.com",
+  projectId: "eventos-93f41",
+  storageBucket: "eventos-93f41.appspot.com",
+  messagingSenderId: "12895592682",
+  appId: "1:12895592682:web:637b05a045fd70f0e87793"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+
 function Login() {
+
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState(''); 
+
+    function logar() {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(result => {
+            alert("ok");
+        }).catch(error => {
+            alert("deu ruim");
+        });
+        
+    }
     return (
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
                 <div className='text-center mb-4'>
                     <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-                    <h1 class="h3 mb-3 fw-normal text-white font-weight-bold">Login</h1>
+                    <h1 className="h3 mb-3 fw-normal text-white font-weight-bold">Login</h1>
                 </div>
 
-                <input type="email" class="form-control my-2" id="floatingInput" placeholder="Digite seu e-mail" />
+                <input onChange={ (e) => setEmail(e.target.value) } type="email" class="form-control my-2" id="inputEmail" placeholder="Digite seu e-mail" />
 
-                <input type="password" className="form-control my-2" id="floatingPassword" placeholder="Digite Sua Senha" />
+                <input onChange={ (e) => setPassword(e.target.value) } type="password" className="form-control my-2" id="inputPassword" placeholder="Digite Sua Senha" />
 
-                <button className="w-100 btn btn-lg btn-login" type="submit">Entrar</button>
+                <button onClick={logar} className="w-100 btn btn-lg btn-login" type="button">Entrar</button>
 
                 <div className='msg-login text-white text-center my-2'>
                         <span><strong>Ebaaa \0/</strong> Login realizado com Sucesso! &#128526;</span>
