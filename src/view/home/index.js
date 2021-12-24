7424a483a8921b16b42e3479e00e19bee9d697b8
heitordeve <heitor.developer@outlook.com>
2021-12-24 05:41:18 -0300

    Dev - Created Home view

---
+++ src/view/home/index.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './index.css';
+import {Link} from 'react-router-dom';
+import Navbar from '../../components/navbar/';
+
+function Home(){
+    return(
+        <Navbar/>
+    )
+}
+
+export default Home;
